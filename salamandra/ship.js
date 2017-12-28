db0132cf93f39d44b49686b034dc8431d63219c2
Ville Tissari <ville.tissari@aalto.fi>
2017-12-28 17:41:58 +0200

    first shooting test

--- salamandra/ship.js
+++ salamandra/ship.js
@@ -1,49 +1,52 @@
 //
 class Ship {
 
+  var spaceKey;
+  var game;
+  var cursors;
+
   constructor(game) {
-    this.game = game;
+    game = game;
     this.sprite = null;
-    this.cursors = null;
     var shots = [];
   }
 
   preload() {
-    this.game.load.image('bg', 'salamandra/img/space_bg.png');
-    this.game.load.image('ship', 'salamandra/img/ship.png');
-    this.game.load.image('bullet', 'salamandra/img/shot.png');
+    game.load.image('bg', 'salamandra/img/space_bg.png');
+    game.load.image('ship', 'salamandra/img/ship.png');
+    game.load.image('bullet', 'salamandra/img/shot.png');
   }
 
   create() {
-    this.sprite = this.game.add.sprite(0,0, 'ship');
-    this.game.physics.enable(this.sprite);
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.sprite = game.add.sprite(0,0, 'ship');
+    game.physics.enable(this.sprite);
+    cursors = game.input.keyboard.createCursorKeys();
   }
 
   update() {
     this.sprite.body.velocity.y = 0;
     this.sprite.body.velocity.x = 0;
 
-    if (this.cursors.left.isDown) {
+    if (cursors.left.isDown) {
       this.sprite.body.velocity.x -= 80;
     }
-    if (this.cursors.right.isDown) {
+    if (cursors.right.isDown) {
       this.sprite.body.velocity.x += 80;
     }
-    if (this.cursors.up.isDown) {
+    if (cursors.up.isDown) {
       this.sprite.body.velocity.y -= 80;
     }
-    if (this.cursors.down.isDown) {
+    if (cursors.down.isDown) {
       this.sprite.body.velocity.y += 80;
     }
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACE)) {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.SPACE)) {
       shoot();
     }
   }
 
   shoot() {
-    var bullet = this.game.add.sprite(this.sprite.x + 32, this.sprite.y + 8,'bullet');
-    this.game.physics.enable(bullet);
+    var bullet = game.add.sprite(this.sprite.x + 32, this.sprite.y + 8,'bullet');
+    game.physics.enable(bullet);
     shots.push(bullet);
 
   }
