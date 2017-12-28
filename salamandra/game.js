a64908701f9e4b227b0b34fc6849f31458b57e28
Ville Tissari <ville.tissari@aalto.fi>
2017-12-29 00:20:10 +0200

    added enemies

--- salamandra/game.js
+++ salamandra/game.js
@@ -80,7 +80,7 @@ window.onload = function() {
       //Create dummy enemy
       enemies.add(game.add.sprite(300, 150, 'dummy'));
       //Create enemies from object layer of the Tilemap
-      map.createFromObjects('Objects', 1 ,'dummy');
+      map.createFromObjects('Objects', 10 ,'dummy');
 
     }
 
